83bdc1c0ed0c7a8a97495caf934aa427c4270809
MiguelSlzr <122295163+MiguelSlzr@users.noreply.github.com>
2023-01-24 20:44:05 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,20 +3,72 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Desea circulo o cuadrado:");
-		String line = scan.nextLine();
+		String opc = scan.nextLine();
 		
-		if (line == "circulo") {
-			System.out.println("Elegiste circulo");
+/*		System.out.println(opc + " <--- esto es lo que contiene el String llamado figura");
+        System.out.println((opc.contains("cuadrado")) + " <--- esto es usando el método String.Contains()" );        
+        System.out.println((opc.equals("cuadrado")) + " <--- esto es usando el método String.Equals()");
+        System.out.println((opc == "cuadrado") + " <--- esto es usando ==");
+ */
+		if (opc.equals("cuadrado")) {
+			System.out.println("Cuanto mide el lado:");
 			
+			String lado2 = scan.nextLine();
+			int lado = Integer.parseInt(lado2);
+			
+			System.out.println("Que quieres calcular:");
+			System.out.println("1. La diagonal");
+			System.out.println("2. El area");
+			System.out.println("3. El perimetro");
+			
+			String dec2 = scan.nextLine();
+			int dec = Integer.parseInt(dec2);
+			
+			if(dec == 1) {
+				double diag = Math.sqrt(2*(lado*lado));
+				System.out.println(diag);
+			}
+			if(dec == 2) {
+				int area = lado*lado;
+				System.out.println(area);
+			}
+			if(dec == 3) {
+				int peri = lado*4;
+				System.out.println(peri);
+			}
 		}
-		if (line == "cuadrado") {
-			System.out.println("Elegiste cuadrado");
+		
+		if (opc.equals("circulo")) {
+			System.out.println("Cuanto mide el radio:");
+			
+			String radio2 = scan.nextLine();
+			int radio = Integer.parseInt(radio2);
+			
+			System.out.println("Que quieres calcular:");
+			System.out.println("1. La circunferencia");
+			System.out.println("2. El area");
+			
+			String dec2 = scan.nextLine();
+			int dec = Integer.parseInt(dec2);
+		
+		//	System.out.println(dec + "");
+	      
+
+			if(dec == 1) {
+				double circ = Math.PI*2*radio;
+				System.out.println(circ);
+			}
+			if(dec == 2) {
+				double area = Math.PI*radio*radio;
+				System.out.println(area);
+			}
+	
 			
 		}
 	}
 		//int num = Integer.parseInt(line);
 		
 }
-
