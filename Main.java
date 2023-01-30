65d791dbcd341650169afe6c72d6d63608059cd4
MiguelSlzr <122295163+MiguelSlzr@users.noreply.github.com>
2023-01-30 10:48:01 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,70 +5,74 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Escoja círculo o cuadrado:");
-		String opc = scan.nextLine();
-		
-/*		System.out.println(opc + " <--- esto es lo que contiene el String llamado figura");
-        System.out.println((opc.contains("cuadrado")) + " <--- esto es usando el método String.Contains()" );        
-        System.out.println((opc.equals("cuadrado")) + " <--- esto es usando el método String.Equals()");
-        System.out.println((opc == "cuadrado") + " <--- esto es usando ==");
- */
-		if (opc.equals("cuadrado")) {
-			System.out.println("¿Cuánto mide el lado del cuadrado?");
-			
-			String lado2 = scan.nextLine();
-			int lado = Integer.parseInt(lado2);
-			Cuadrado miCuadrado = new Cuadrado(lado);
-			
-			
-			System.out.println("¿Qué desea calcular?");
-			System.out.println("1. La diagonal");
-			System.out.println("2. El área");
-			System.out.println("3. El perímetro");
-			
-			String dec2 = scan.nextLine();
-			int dec = Integer.parseInt(dec2);
-			
-			
-			if(dec == 1) {
-				System.out.println(miCuadrado.laDiagonal());
 
-			} else if(dec == 2) {
-				System.out.println(miCuadrado.elArea());
-			} else if(dec == 3) {
-				System.out.println(miCuadrado.elPerimetro());
-			} else {
-				System.out.println("Lo siento, solo se puede escoger 1, 2 o 3.");
-			}
-			
-		} else if (opc.equals("círculo")) {
-			System.out.println("Cuanto mide el radio:");
-			
-			String radio2 = scan.nextLine();
-			int radio = Integer.parseInt(radio2);
-			Circulo miCirculo = new Circulo(radio);
-			
-			
-			
-			System.out.println("Que quieres calcular:");
-			System.out.println("1. La circunferencia");
-			System.out.println("2. El área");
-			
-			String dec2 = scan.nextLine();
-			int dec = Integer.parseInt(dec2);
+		boolean bandera = true;
 		
-	      
-
-			if(dec == 1) {
-				System.out.println(miCirculo.laCircunferencia());
-			} else if(dec == 2) {
-				System.out.println(miCirculo.elArea());
-			} else {
-				System.out.println("Lo siento, solo se puede escoger 1 o 2.");
-			}
+		
+		while(bandera == true) {
+			System.out.println("Ingrese un número acorde a las siguientes opciones:");
+			System.out.println("");
 			
-		} else {
-			System.out.println("Esa no es una opción, inténtelo de nuevo.");
+			System.out.println("1. Calcular la diagonal de un cuadrado");
+			System.out.println("2. Calcular el perímetro de un cuadrado");
+			System.out.println("3. Calcular el área de un cuadrado");
+			System.out.println("4. Calcular el perímetro de un círculo");
+			System.out.println("5. Calcular el área de un círculo");
+			System.out.println("6. Salir del menú");
+			int decision = Integer.parseInt(scan.nextLine());
+			
+			int lado;
+			int radio;
+			Cuadrado miCuadrado = new Cuadrado();
+			Circulo miCirculo = new Circulo();
+			
+			switch (decision) {
+				case 1:
+					System.out.println("Escriba el valor del lado del cuadrado:");
+					lado = Integer.parseInt(scan.nextLine());
+					
+					miCuadrado.lado = lado;
+					
+					System.out.println("El lado del cuadrado es: " + miCuadrado.laDiagonal());
+					System.out.println("");
+					break;
+				case 2:
+					System.out.println("Escriba el valor del lado del cuadrado:");
+					lado = Integer.parseInt(scan.nextLine());
+					miCuadrado.lado = lado;
+					System.out.println("El lado del cuadrado es: " + miCuadrado.elPerimetro());
+					System.out.println("");
+					break;
+				case 3:
+					System.out.println("Escriba el valor del lado del cuadrado:");
+					lado = Integer.parseInt(scan.nextLine());
+					miCuadrado.lado = lado;
+					System.out.println("El lado del cuadrado es: " + miCuadrado.elArea());
+					System.out.println("");
+					break;
+				case 4:
+					System.out.println("Escriba el valor del radio del círculo:");
+					radio = Integer.parseInt(scan.nextLine());
+					miCirculo.radio = radio;
+					System.out.println("La circunferencia del círculo es: " + miCirculo.elArea());
+					System.out.println("");
+					break;
+				case 5:
+					System.out.println("Escriba el valor del radio del círculo:");
+					radio = Integer.parseInt(scan.nextLine());
+					miCirculo.radio = radio;
+					System.out.println("La circunferencia del círculo es: " + miCirculo.laCircunferencia());
+					System.out.println("");
+					break;
+				case 6:	
+					System.out.println("Saliendo...");
+					bandera = false;
+					break;
+				default:
+					System.out.println("Por favor elija una opción válida.");
+					System.out.println("");
+					break;
+			}
 		}
 		scan.close();
 	}	
